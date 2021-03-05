fd7f07debc0c1cf9342269aaa0ed529a64a92af9
Guilherme Henrique <76486471+GuinhoHenriq@users.noreply.github.com>
2021-03-05 10:06:58 -0300

    Add files via upload

---
+++ RoboCotacao/ClassModelo/ClienteCotacao.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RoboCotacaoBradesco.ClassModelo
+{
+    class ClienteCotacao
+    {
+
+        #region Informações da classe
+        /*
+         * Autor:   Guilherme Henrique - 04/12/2020
+         * Obs.:    Essa classe representa o modelo da table #TB_COTACAO (SERVER TMKT_ZL_DB26 atualmente)
+         *          Procedure - STP_CARREGA_MAILING_ROBO_COTACAO
+                    Os dados dessa table representa os usuários que devem receber uma cotação automática.
+         */
+        #endregion
+
+
+        #region Propriedades da Classe
+
+        public int Cliente_SEQ { get; set; }
+        public string Cliente_Clicodigo { get; set; }
+        public string Cliente_CPF_CNPJ { get; set; }
+        public string Cliente_Nome { get; set; }
+        public int Cliente_DDD { get; set; }
+        public int Cliente_Telefone { get; set; }
+        public string Cliente_campanha { get; set; }
+        public int Cliente_Conta {get; set;} 
+        public int Cliente_Sucursal {get; set;}
+        public int Cliente_Apolice {get; set;}
+        public int Cliente_Item {get; set;}
+        public DateTime Cliente_DtFimVigencia {get; set;}
+        public string Cliente_Matricula { get; set; }
+        public string Cliente_NomeWorksite { get; set;}
+        public int Cliente_CEP { get; set;}
+        public int Cliente_Agencia { get; set;}
+        public string Cliente_Email { get; set; }
+
+
+        #endregion 
+       
+    }
+}
---
+++ RoboCotacao/RoboCotacao.Designer.cs
@@ -0,0 +1,183 @@
+﻿namespace RoboCotacaoBradesco
+{
+    partial class RoboCotacao
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(RoboCotacao));
+            this.btnIniciar = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtRecebido = new System.Windows.Forms.TextBox();
+            this.txtProcessado = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtNProcessado = new System.Windows.Forms.TextBox();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnIniciar
+            // 
+            this.btnIniciar.BackColor = System.Drawing.Color.Honeydew;
+            this.btnIniciar.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIniciar.Location = new System.Drawing.Point(12, 17);
+            this.btnIniciar.Name = "btnIniciar";
+            this.btnIniciar.Size = new System.Drawing.Size(110, 58);
+            this.btnIniciar.TabIndex = 0;
+            this.btnIniciar.Text = "Iniciar Robo";
+            this.btnIniciar.UseVisualStyleBackColor = false;
+            this.btnIniciar.Click += new System.EventHandler(this.btnIniciar_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 128);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(354, 381);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 101);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(354, 21);
+            this.progressBar1.TabIndex = 2;
+            this.progressBar1.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(128, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(147, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Qntd Importado da base:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(128, 40);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(175, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Qntd Processado c/ sucesso:";
+            // 
+            // txtRecebido
+            // 
+            this.txtRecebido.Location = new System.Drawing.Point(309, 9);
+            this.txtRecebido.Name = "txtRecebido";
+            this.txtRecebido.Size = new System.Drawing.Size(45, 20);
+            this.txtRecebido.TabIndex = 5;
+            this.txtRecebido.Text = "0";
+            // 
+            // txtProcessado
+            // 
+            this.txtProcessado.Location = new System.Drawing.Point(309, 37);
+            this.txtProcessado.Name = "txtProcessado";
+            this.txtProcessado.Size = new System.Drawing.Size(45, 20);
+            this.txtProcessado.TabIndex = 6;
+            this.txtProcessado.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(128, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Qntd Processado s/ sucesso:";
+            // 
+            // txtNProcessado
+            // 
+            this.txtNProcessado.Location = new System.Drawing.Point(309, 66);
+            this.txtNProcessado.Name = "txtNProcessado";
+            this.txtNProcessado.Size = new System.Drawing.Size(45, 20);
+            this.txtNProcessado.TabIndex = 8;
+            this.txtNProcessado.Text = "0";
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.BackColor = System.Drawing.Color.Honeydew;
+            this.btnCancelar.Font = new System.Drawing.Font("MingLiU_HKSCS-ExtB", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancelar.Location = new System.Drawing.Point(12, 17);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(110, 58);
+            this.btnCancelar.TabIndex = 9;
+            this.btnCancelar.Text = "Cancelar e Fechar";
+            this.btnCancelar.UseVisualStyleBackColor = false;
+            this.btnCancelar.Visible = false;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // RoboCotacao
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.ForestGreen;
+            this.ClientSize = new System.Drawing.Size(378, 521);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.txtNProcessado);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txtProcessado);
+            this.Controls.Add(this.txtRecebido);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.btnIniciar);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "RoboCotacao";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Robo Cotacao Bradesco";
+            this.Load += new System.EventHandler(this.RoboCotacao_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnIniciar;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtRecebido;
+        private System.Windows.Forms.TextBox txtProcessado;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtNProcessado;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
